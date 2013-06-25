cb8be91ec79fd87339a63bf142bccfc2d275ab7c
Leigh Hunt <leigh.hunt@splicegroup.co.nz>
2013-06-25 20:55:41 +1200

    Adding backbone code and UI in entirety from https://github.com/ccoenraets/nodecellar

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 var express = require('express'),
+    path = require('path'),
+    http = require('http'),
     wine = require('./routes/wines');
- 
+
 var app = express();
- 
+
 app.configure(function () {
-    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser());
+    app.set('port', process.env.PORT || 3000);
+    app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser()),
+    app.use(express.static(path.join(__dirname, 'public')));
 });
- 
+
 app.get('/wines', wine.findAll);
 app.get('/wines/:id', wine.findById);
 app.post('/wines', wine.addWine);
 app.put('/wines/:id', wine.updateWine);
 app.delete('/wines/:id', wine.deleteWine);
- 
-app.listen(3000);
-console.log('Listening on port 3000...');
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
+});
